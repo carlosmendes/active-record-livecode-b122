5330f74ec0c5636f84c84b4c376de1d6ccb1edfa
Carlos Mendes <carls.mendes@gmail.com>
2018-02-05 13:29:18 +0000

    initial livecode

---
+++ app.rb
@@ -0,0 +1,43 @@
+require_relative "config/application"
+
+require "sinatra"
+require "sinatra/reloader"
+require "sinatra/activerecord"
+
+set :database, { adapter: "sqlite3", database: "db/development.sqlite3" }
+set :views, Proc.new { File.join(root, "app/views") }
+
+# Routes + Controller
+
+get '/about' do
+  erb :about # Embedded Ruby -> renders about.erb from the views folder
+end
+
+get '/' do
+  # get all restaurants from DB and save it in @restaurants
+
+  erb :index
+end
+
+get '/restaurants/new' do
+  erb :new
+end
+
+post '/restaurants/create' do
+  # get name and description from the params hash
+
+  # build a new restaurant object with the saved name and description
+
+  # save the object into the DB
+
+  # redirect to the list of restaurants
+  redirect to("/")
+end
+
+get '/restaurants/:id' do
+  # get id from the params hash
+
+  # find the restaurant object from the DB and save it in @restaurant
+
+  erb :show
+end
